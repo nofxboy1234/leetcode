bfbc3b4f47c8272860c3e5b71a27fe61f4ab3872
Dylan Palmboom <nofxboy1234@gmail.com>
2025-05-21 21:05:28 +0200

    Add first leetcode exercise

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-import hello from "./hello.js";
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
 
-hello();
+var runningSum2 = function (nums) {
+  return nums.map((num, index) =>
+    nums.slice(0, index + 1).reduce((sum, cur) => sum + cur, 0)
+  );
+};
+
+var runningSum = function (nums) {
+  const result = [];
+
+  nums.forEach((num, index) => {
+    const runSlice = nums.slice(0, index + 1);
+    const runSum = runSlice.reduce((sum, cur) => sum + cur);
+
+    result.push(runSum);
+  });
+
+  return result;
+};
+
+const result = runningSum2([1, 2, 3, 4]);
+console.log(result);
