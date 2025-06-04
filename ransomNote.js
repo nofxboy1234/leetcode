e1ab42c2f861857b5436fa40b778b1024a56cc3e
Dylan Palmboom <nofxboy1234@gmail.com>
2025-06-04 22:38:41 +0200

    Add ransom note

---
+++ ransomNote.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function (ransomNote, magazine) {
+  const ransomNoteChars = {};
+  const magazineChars = {};
+
+  ransomNote
+    .split("")
+    .forEach((letter) =>
+      ransomNoteChars[letter]
+        ? (ransomNoteChars[letter] += 1)
+        : (ransomNoteChars[letter] = 1)
+    );
+
+  magazine
+    .split("")
+    .forEach((letter) =>
+      magazineChars[letter]
+        ? (magazineChars[letter] += 1)
+        : (magazineChars[letter] = 1)
+    );
+
+  console.log(ransomNoteChars);
+  console.log(magazineChars);
+
+  const result = Object.keys(ransomNoteChars).map((char) => {
+    if (!magazineChars[char]) {
+      return false;
+    }
+
+    if (magazineChars[char] < ransomNoteChars[char]) {
+      return false;
+    }
+
+    return true;
+  });
+
+  return result.every((check) => check === true);
+};
+
+// canConstruct("a", "b");
