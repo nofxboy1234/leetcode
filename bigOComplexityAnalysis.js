c25409bd5e11f15e231061b2639f0889279749b4
Dylan Palmboom <nofxboy1234@gmail.com>
2025-06-18 15:13:50 +0200

    Add factorial iterative implementation;

--- bigOComplexityAnalysis.js
+++ bigOComplexityAnalysis.js
@@ -270,3 +270,17 @@ function getPermutations(arr) {
 const cities = ["Copenhagen", "Stockholm", "Oslo"];
 const result3 = getPermutations(cities);
 console.log(result3);
+
+// Time: O(n), Space: O(1)
+function factorial(n) {
+  if (n < 0) return undefined;
+  if (n === 0 || n === 1) return 1;
+
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(3));
