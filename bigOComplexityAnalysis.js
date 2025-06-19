a3b6083d1472914de8eb43305c668675875aa28c
Dylan Palmboom <nofxboy1234@gmail.com>
2025-06-19 13:48:40 +0200

    Add Two Sum solutions using Unordered Array

--- bigOComplexityAnalysis.js
+++ bigOComplexityAnalysis.js
@@ -521,3 +521,50 @@ OUTPUT:
   console.log(fibonacciRecursive(4));
   // OUTPUT => 3
 }
+
+// Two Sum
+{
+  // Time: O(n^2), Space: O(1)
+  const twoSumBrute = (list, target) => {
+    for (let i = 0; i < list.length; i++) {
+      for (let j = 0; j < list.length; j++) {
+        if (list[i] + list[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  };
+
+  const unsortedNrs = [4, 2, 6, 3, 1, 5, 9, 7, 8, 10];
+  console.log(twoSumBrute(unsortedNrs, 7));
+  // OUTPUT => [0, 3]
+}
+
+{
+  const twoSumHashmap = (list, target) => {
+    // Time: O(1), Space: O(1)
+    const lookupTable = {};
+
+    // build a lookup table
+    // Time: O(n), Space: O(n)
+    for (let i = 0; i < list.length; i++) {
+      lookupTable[list[i]] = i;
+    }
+
+    // iterate
+    // Time: O(n)
+    for (let j = 0; j < list.length; j++) {
+      // Time: O(1), Space: O(1)
+      let diff = target - list[j];
+      // Time: O(1)
+      if (lookupTable[diff] && lookupTable[diff] !== j) {
+        return [j, lookupTable[diff]];
+      }
+    }
+  };
+  // Time: O(1) + O(n) + O(n) + O(1) + O(1) => O(n)
+  // Space: O(1) + O(n) + O(1) => O(n)
+
+  const unsortedNrs = [4, 2, 6, 3, 1, 5, 9, 7, 8, 10];
+  console.log(twoSumHashmap(unsortedNrs, 7));
+}
