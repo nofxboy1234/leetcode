6f0f5d3f0af2e7892889e4cf1ae3adf0e15f82b1
Dylan Palmboom <nofxboy1234@gmail.com>
2025-06-24 10:09:54 +0200

    Fix trappingRainWater algo

--- trappingRainWater.js
+++ trappingRainWater.js
@@ -11,6 +11,10 @@ var trap = function (height) {
   let waterBlocks = 0;
 
   while (left >= 0 && left < height.length - 1) {
+    if (right === left) {
+      break;
+    }
+
     if (height[right] >= height[left]) {
       // hit a wall
       // calculate max water depth between left and right
@@ -29,9 +33,38 @@ var trap = function (height) {
       right++;
     } else if (right === height.length - 1) {
       // reached the end
-      // go down 1 step
-      left++;
-      right = left + 1;
+      // save left's position to come back to
+      const originalLeftIndex = left;
+      // go right to left
+      left = right - 1;
+
+      while (right != left) {
+        if (height[left] >= height[right]) {
+          // hit a wall
+          // calculate max water depth between right and left
+          const maxWaterDepth = Math.min(height[right], height[left]);
+          // move right pointer to left pointer and get depth at each index along the way
+          if (maxWaterDepth === 0) {
+            right = left;
+          }
+
+          while (right > left) {
+            const currentWaterDepth = maxWaterDepth - height[right];
+            waterBlocks += currentWaterDepth;
+            right--;
+          }
+
+          if (left === originalLeftIndex) {
+            // finished counting water from right to left
+            break;
+          }
+
+          left--;
+        } else {
+          // keep searching for a wall hit
+          left--;
+        }
+      }
     } else {
       // keep searching for a wall hit
       right++;
@@ -41,6 +74,14 @@ var trap = function (height) {
   return waterBlocks;
 };
 
-const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
-const result = trap(height);
-console.log(result);
+{
+  const height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1];
+  const result = trap(height);
+  console.log(result);
+}
+
+{
+  const height = [4, 2, 3];
+  const result = trap(height);
+  console.log(result);
+}
